7f20bbb81b19467a577cbbd56c6c4d371f173981
yarops <ed.creater@gmail.com>
2024-09-18 21:07:15 +0300

    Change counters scripts load strategy

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,13 +14,10 @@ import { getAllMenus } from 'lib/menus';
 import 'styles/globals.scss';
 import 'styles/wordpress.scss';
 import variables from 'styles/_variables.module.scss';
-import Script from 'next/script';
 import { useState } from 'react';
 import { ApolloProvider } from "@apollo/client";
 import { getApolloClient } from 'lib/apollo-client';
 import PrivacyBox from 'components/PrivacyBox';
-import Link from 'next/link';
-import Image from 'next/image';
 
 function App({ Component, pageProps = {}, metadata, recentPosts, categories, menus, sidePosts }) {
   const [isNavOpen, setisNavOpen] = useState();
@@ -53,42 +50,6 @@ function App({ Component, pageProps = {}, metadata, recentPosts, categories, men
             <meta name="google-site-verification" content="zVS51G9qsrXjF_p5P-xZD1xC1HtpsPypwR0o1pqtXkU" />
           </Head>
           <Component {...pageProps} />
-
-          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-PHD37JG2C4" />
-          <Script id="gtm" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-PHD37JG2C4');
-              `}
-          </Script>
-
-          <Script id="livecounter" strategy="afterInteractive">
-            {`
-              (function(d,s){d.getElementById("licntE007").src="https://counter.yadro.ru/hit?t44.18;r"+escape(d.referrer)+((typeof(s)=="undefined")?"":";s"+s.width+"*"+s.height+"*"+(s.colorDepth?s.colorDepth:s.pixelDepth))+";u"+escape(d.URL)+";h"+escape(d.title.substring(0,150))+";"+Math.random()})(document,screen)
-            `}
-          </Script>
-          <Link
-            href="https://www.liveinternet.ru/click"
-            target={'_blank'}
-            style={{
-              position: "absolute",
-              zIndex: "-100",
-              transform: "translate(-100%)",
-            }}
-          >
-            <Image
-              id={'licntE007'}
-              priority
-              src={'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7'}
-              alt="alt"
-              width="31"
-              height="31"
-            />
-          </Link>
-
           {isPrivacyOpen && <PrivacyBox />}
         </SearchProvider>
       </SiteContext.Provider>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,9 @@
 /* eslint-disable @next/next/no-document-import-in-page */
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { Helmet } from 'react-helmet';
+import Script from 'next/script';
+import Link from 'next/link';
+import Image from 'next/image';
 
 // Via https://github.com/vercel/next.js/blob/canary/examples/with-react-helmet/pages/_document.js
 
@@ -37,9 +40,47 @@ export default class MyDocument extends Document {
         <Head>
           {this.helmetHeadComponents}
         </Head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-PHD37JG2C4"
+          strategy="beforeInteractive"
+        />
+        <Script id="gtm" strategy="beforeInteractive">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-PHD37JG2C4');
+              `}
+        </Script>
+
+        <Script id="livecounter" strategy="beforeInteractive">
+          {`
+              (function(d,s){d.getElementById("licntE007").src="https://counter.yadro.ru/hit?t44.18;r"+escape(d.referrer)+((typeof(s)=="undefined")?"":";s"+s.width+"*"+s.height+"*"+(s.colorDepth?s.colorDepth:s.pixelDepth))+";u"+escape(d.URL)+";h"+escape(d.title.substring(0,150))+";"+Math.random()})(document,screen)
+            `}
+        </Script>
         <body {...this.helmetBodyAttrComponents}>
           <Main />
           <NextScript />
+          <Link
+            href="https://www.liveinternet.ru/click"
+            target={'_blank'}
+            style={{
+              position: "absolute",
+              zIndex: "-100",
+              transform: "translate(-100%)",
+            }}
+          >
+            <Image
+              id={'licntE007'}
+              priority
+              src={'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7'}
+              alt="alt"
+              width="31"
+              height="31"
+            />
+          </Link>
         </body>
       </Html>
     );
