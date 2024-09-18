a2e6dba0429e6452f383216c2e1253962870be55
yarops <ed.creater@gmail.com>
2024-09-18 21:43:43 +0300

    Move script initialization in app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -19,6 +19,10 @@ import { ApolloProvider } from "@apollo/client";
 import { getApolloClient } from 'lib/apollo-client';
 import PrivacyBox from 'components/PrivacyBox';
 
+import Script from 'next/script';
+import Link from 'next/link';
+import Image from 'next/image';
+
 function App({ Component, pageProps = {}, metadata, recentPosts, categories, menus, sidePosts }) {
   const [isNavOpen, setisNavOpen] = useState();
   const [isPrivacyOpen = true, setisPrivacyOpen] = useState();
@@ -50,6 +54,61 @@ function App({ Component, pageProps = {}, metadata, recentPosts, categories, men
             <meta name="google-site-verification" content="zVS51G9qsrXjF_p5P-xZD1xC1HtpsPypwR0o1pqtXkU" />
           </Head>
           <Component {...pageProps} />
+
+          <Script
+            id="gtm"
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-PHD37JG2C4"
+            strategy="afterInteractive"
+            onLoad={() => {
+              console.log('gtm loaded')
+            }}
+          />
+          <Script
+            id="gtmCounter"
+            strategy="afterInteractive"
+            onReady={() => {
+              console.log('gtag loaded')
+            }}
+          >
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-PHD37JG2C4');
+              `}
+          </Script>
+
+          <Script
+            id="livecounter"
+            strategy="afterInteractive"
+            onReady={() => {
+              console.log('livecounter loaded')
+            }}
+          >
+            {`(function(d,s){d.getElementById("licntE007").src="https://counter.yadro.ru/hit?t44.18;r"+escape(d.referrer)+((typeof(s)=="undefined")?"":";s"+s.width+"*"+s.height+"*"+(s.colorDepth?s.colorDepth:s.pixelDepth))+";u"+escape(d.URL)+";h"+escape(d.title.substring(0,150))+";"+Math.random()})(document,screen)`}
+          </Script>
+
+          <Link
+            href="https://www.liveinternet.ru/click"
+            target={'_blank'}
+            style={{
+              position: "absolute",
+              zIndex: "-100",
+              transform: "translate(-100%)",
+            }}
+          >
+            <Image
+              id={'licntE007'}
+              priority
+              src={'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7'}
+              alt="alt"
+              width="31"
+              height="31"
+            />
+          </Link>
+
           {isPrivacyOpen && <PrivacyBox />}
         </SearchProvider>
       </SiteContext.Provider>
