98e0f746b0df6b9d05982777cea79a6d14dcd430
yarops <ed.creater@gmail.com>
2024-09-19 12:40:28 +0300

    Add 16 items top games

--- src/data/posts.js
+++ src/data/posts.js
@@ -116,7 +116,7 @@ export const QUERY_ALL_POSTS = gql`
 export const QUERY_TOP_POSTS = gql`
   ${POST_FIELDS}
   query TopPosts {
-    posts(first: 10, where: {topMeta: 1}) {
+    posts(first: 16, where: {topMeta: 1}) {
       edges {
         node {
           ...PostFields
